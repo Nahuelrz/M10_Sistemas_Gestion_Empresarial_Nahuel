09ac8c86e3dd13ae9f6d19af486b7f899638a0ff
Kevin Nahuel Ramírez Murieda <kevinmurieda@alumnes.ilerna.com>
2026-02-08 21:59:35 +0100

    Add views

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -19,15 +19,16 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Education',
-    'version': '1.0',
+    'version': '1.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
+        'views/menu.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
