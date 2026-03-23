09ddae8121b675c52cbecdc34c28f2aa39f2ec1d
Kevin Nahuel Ramírez Murieda <kevinmurieda@alumnes.ilerna.com>
2026-03-23 18:51:01 +0100

    feat

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -14,6 +14,7 @@
 
     'author': "My Company",
     'website': "https://www.yourcompany.com",
+    'license': 'LGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -26,6 +27,7 @@
 
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/menu.xml',
